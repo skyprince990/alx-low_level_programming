f9e9b3ab4a7860fb4c269adcbf8c9de164acec5a
arthur <imanshyuba@gmail.com>
2023-03-16 09:43:57 -0700

    joo

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
- *this is shit 
+ * main - Entry point
  *
- *return 0 
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-		return (0); }
+	return (0);
+}
+
