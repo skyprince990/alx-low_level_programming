73ab3616b17ec861d35d356052852d476840fa69
arthur <imanshyuba@gmail.com>
2023-03-17 07:48:46 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,20 +1,19 @@
 #include <stdio.h>
 /**
- *main - This will print lower case then upper case 
- * 
+ * main - This will print lower case then upper case 
  * Return: Always 0 (succes)
  */
 int main(void)
 {
 	int i;
 	int j;
-	char lower[26] ="abcdefghijklmnopqrstuvwxyz";
-	char upper[26] ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	for (i=0;i<26;i++)
+	char lower[26] = "abcdefghijklmnopqrstuvwxyz";
+	char upper[26] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	for (i = 0;i < 26;i++)
 	{
 		putchar(lower[i]);
 	}
-	for (j=0;j<26;j++)
+	for (j = 0; j < 26;j++)
 	{
 		putchar(upper[j]);
 	}
